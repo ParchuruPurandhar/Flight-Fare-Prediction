40655ab0e4e5a482f110095e45cdce95d279886e
Parchuru Purandhar <139546072+ParchuruPurandhar@users.noreply.github.com>
2026-06-05 12:41:35 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -19,18 +19,30 @@ journey_month = st.slider("Journey Month", 1, 12, 6)
 journey_year = st.number_input("Journey Year", value=2026)
 
 duration_time = st.number_input("Duration (Minutes)", min_value=30)
+total_stops = st.number_input("Total Stops", min_value=0, max_value=4, value=1)
+
+dep_hour = st.number_input("Departure Hour", min_value=0, max_value=23, value=10)
+dep_min = st.number_input("Departure Minute", min_value=0, max_value=59, value=0)
+
+arr_hour = st.number_input("Arrival Hour", min_value=0, max_value=23, value=12)
+arr_min = st.number_input("Arrival Minute", min_value=0, max_value=59, value=0)
 
 if st.button("Predict Fare"):
 
     features = pd.DataFrame({
-        "Airline":[airline],
-        "Source":[source],
-        "Destination":[destination],
-        "Journey_Day":[journey_day],
-        "Journey_Month":[journey_month],
-        "Journey_Year":[journey_year],
-        "Duration_Time":[duration_time]
-    })
+    "Airline":[airline],
+    "Source":[source],
+    "Destination":[destination],
+    "Total_Stops":[total_stops],
+    "Journey_Day":[journey_day],
+    "Journey_Month":[journey_month],
+    "Journey_Year":[journey_year],
+    "Duration_Time":[duration_time],
+    "Dep_Hour":[dep_hour],
+    "Dep_Min":[dep_min],
+    "Arr_Hour":[arr_hour],
+    "Arr_Min":[arr_min]
+})
 
     prediction = model.predict(features)[0]
 
